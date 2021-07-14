f82275c34d233752e01706b5684ffef65733864e
John Kirkwood (kij) <kij@odoo.com>
2021-07-14 17:43:03 +0100

    [IMP] override logging level for missing attachments

--- custom_error_logs/models/ir_attachment.py
+++ custom_error_logs/models/ir_attachment.py
@@ -8,10 +8,18 @@ from odoo.tools.parse_version import parse_version
 
 
 @contextmanager
-def log_level(temp_level):
-    """Context manager to override root_logger.level for the scope of the block.
+def log_level(temp_level=logging.WARNING):
+    """Context manager to override the :class:`logging` root :class:`logging.Logger`
+    logging level for the scope of the context block.
 
-    It can be used to hide expected log messages that are polluting the logs.
+    It can be used to hide expected log messages that are polluting the logs by
+    increasing the log level to ``WARNING`` to hide ``INFO`` or ``DEBUG`` logs.
+
+    This does not override the individual :class:`logging.Logger` :class:`logging.Handler`s
+    so these may log lower-level messages through various channels.
+
+    :param temp_level: root logger logging level for the context block 
+    :type temp_level: int, optional
     """
     _logger = logging.getLogger()
     orig_level = _logger.level
