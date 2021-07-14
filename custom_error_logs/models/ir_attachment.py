9eb9b51c211d53ab2af6df35bceec2ed1954fdba
John Kirkwood (kij) <kij@odoo.com>
2021-07-14 17:01:55 +0100

    [IMP] override logging level for missing attachments

--- custom_error_logs/models/ir_attachment.py
+++ custom_error_logs/models/ir_attachment.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
-from odoo import api, models
+import logging
 
+from contextlib import contextmanager
+
+from odoo import api, models
 from odoo.tools.parse_version import parse_version
+
+
+@contextmanager
+def log_level(temp_level):
+    """Context manager to override root_logger.level for the scope of the block.
+
+    It can be used to hide expected log messages that are polluting the logs.
+    """
+    _logger = logging.getLogger()
+    orig_level = _logger.level
+    _logger.setLevel(temp_level)
+    try:
+        yield _logger
+    finally:
+        pass
+        _logger.setLevel(orig_level)
 
 
 class IrAttachment(models.Model):
@@ -9,22 +28,16 @@ class IrAttachment(models.Model):
 
     @api.model
     def _file_read(self, fname, bin_size=False):
+        """WARNING: !!! Remember to disable this when debugging attachments !!!
+        """
         self.env.cr.execute("""
             SELECT latest_version 
             FROM ir_module_module 
             WHERE name = 'base';
         """)
         version = parse_version(self.env.cr.fetchone()[0])
-        if int(version[0]) < 14:
-            res = ''
-            try:
+        with log_level(logging.WARNING) as _:
+            if int(version[0]) < 14:
                 return super()._file_read(fname, bin_size)
-            except (IOError, OSError):
-                pass
-            return res
-        else:
-            try:
+            else:
                 return super()._file_read(fname)
-            except (IOError, OSError):
-                pass
-            return b''
